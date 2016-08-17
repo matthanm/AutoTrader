b311b871dd5f714c78cfa2e6d3ab6af6596fddeb
matthanm <matthanmyers@gmail.com>
2016-08-16 19:49:20 -0500

    Added command line inputs
    Addition and removal now possible, logging to files

--- src/AutoTrader.py
+++ src/AutoTrader.py
@@ -1,5 +1,6 @@
 from ElectronicBroker import ElectronicBroker
 import threading
+import sys
 
 __author__ = "Matthan Myers"
 __copyright__ = "Copyright 2016, AutoTrader"
@@ -14,14 +15,45 @@ def main():
 	print("\nWelcome to AutoTrader!\n### Created by " + __author__ + ", " + __copyright__ +
 	".\n### Version " + __version__ +
 	".\n### For questions, comments or suggestions, contact " + __maintainer__ + " at " + __email__ + ".\n-------------------\n\n")
-	f = open('Symbols.txt', 'r')
-	symbols = f.read().split()
+	symbols = []
+	with open('Symbols.cfg', 'r') as f:
+		for line in f:
+			symbols.append(line.split())
 	stocks = []
 	for symbol in symbols:
-		stock = ElectronicBroker(symbol)
+		stock = ElectronicBroker(symbol[0], symbol[1])
 		stocks.append(stock)
-		print("Starting thread for " + symbol + "...")
 		stock.start()
+	print("All Electronic Brokers have been initialized.")
+
+	while True:
+		cmd = input("\nEnter a command: ")
+		if cmd == "exit":
+			for stock in stocks:
+				stock.stop()
+			print("Exiting program...")
+			exit()
+		elif cmd == "add":
+			newSymbol = input("Enter a symbol and exchange to add: ").split()
+			with open('Symbols.cfg', 'a') as f:
+				f.write("\n" + newSymbol[0] + " " + newSymbol[1])
+			newStock = ElectronicBroker(newSymbol[0], newSymbol[1])
+			stocks.append(newStock)
+			newStock.start()
+		elif cmd == "rmv":
+			rmvSymbol = input("Enter a symbol and exchange to remove: ").split()
+			with open('Symbols.cfg', 'r') as f:
+				lines = f.readlines()
+			with open('Symbols.cfg', 'w') as f:
+				for line in lines:
+					if line != rmvSymbol[0] + " " + rmvSymbol[1] and line != "\n":
+						f.write(line)
+			for rmvStock in stocks:
+				if rmvStock.symbol == rmvSymbol[0] and rmvStock.exchange == rmvSymbol[1]:
+					rmvStock.stop()
+					stocks.remove(rmvStock)
+		else:
+			print("\"" + cmd + "\"" + " is not a valid command.")
 
 if __name__ == "__main__":
 	main()
--- src/ElectronicBroker.py
+++ src/ElectronicBroker.py
@@ -4,13 +4,19 @@ import time
 import threading
 
 class ElectronicBroker(threading.Thread):
-	def __init__(self, symbol):
+	def __init__(self, symbol, exchange):
+		print("Initializing an Electronic Broker for " + symbol + "...")
 		super(ElectronicBroker, self).__init__()
 		self.symbol = symbol
+		self.exchange = exchange
+		self.isRunning = True
 	def run(self):
-		while True:
-			data = getQuotes(self.symbol)
-			for item in data:
-				print(item.get("LastTradeDateTime"))
-				print(item.get("LastTradePrice"))
+		while self.isRunning:
+			data = getQuotes(self.exchange + ":" + self.symbol)
+			with open("StockData/" + self.exchange + "." + self.symbol + "-" + time.strftime("%m.%d.%y") + ".dat", "a") as f:
+				for item in data:
+					f.write(item.get("LastTradeDateTime") + " " + item.get("LastTradePrice") + "\n")
 			time.sleep(5)
+	def stop(self):
+		print("Stopping an Electronic Broker for " + self.symbol + "...")
+		self.isRunning = False
